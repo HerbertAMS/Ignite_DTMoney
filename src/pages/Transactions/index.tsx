091ecc1a897cdb367aacfbe79d13986fd60d9089
HerbertAMS <herbert.ams@gmail.com>
2023-02-23 19:17:48 -0300

    dev Summary and table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,38 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
-    return(
-        <div>
-            <Header />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/02/2023</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>20/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
