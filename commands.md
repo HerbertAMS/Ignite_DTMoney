0988819bd9eb20e4cdb621288765d5fc94920dc7
HerbertAMS <herbert.ams@gmail.com>
2023-02-24 10:33:00 -0300

    dev SearchForm

--- commands.md
+++ commands.md
@@ -1,3 +1,5 @@
 # Pacotes instalados
 npm i
 npm i styled-components -D
+npm i phosphor-react
+npm install @radix-ui/react-dialog
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import {
   PriceHighlight,
@@ -11,11 +12,13 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+      
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
-              <td>Desenvolvimento de site</td>
+              <td width={"50%"}>Desenvolvimento de site</td>
               <td>
                 <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
               </td>
@@ -23,7 +26,7 @@ export function Transactions() {
               <td>13/02/2023</td>
             </tr>
             <tr>
-              <td>Hamburguer</td>
+              <td width={"50%"}>Hamburguer</td>
               <td>
                 <PriceHighlight variant="outcome">-R$ 50,00</PriceHighlight>
               </td>
